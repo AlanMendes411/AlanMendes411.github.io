13a1ec2a5cf12cd30fc851de5546c06ddf7538d1
Alan_Mendes <74555018+AlanMendes411@users.noreply.github.com>
2024-03-28 17:36:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-//IMC = peso / altua * altua
-//menor que 16 = muito abaixo do peso
-//menor que 18 = magro
-//entre 18 e 24 = nomral
-//entre 25 e 29 = sobrepeso
-//entre 30 e 40 obesidade
-//maior que 40 = obesidade morbida 
-
 var peso;
 var altura;
 var imc;
@@ -20,23 +12,23 @@ function calcular(event){
     imc = peso / (altura * altura);
     resultado = document.getElementById("resultado");
     
-    if (imc < 16) {
-        resultado.innerHTML = "IMC: " + imc.toFixed(2) + "<br> Você está muito abaixo do peso."
+    if (imc <= 18.5) {
+        resultado.innerHTML = "IMC: " + imc.toFixed(2) + "<br> Você está abaixo do peso."
     }
-    else if (imc > 16 && imc <= 18){
-        resultado.innerHTML = "IMC: " + imc.toFixed(2) + "<br> Você está magro."
-    }
-    else if (imc > 18 && imc <= 24){
+    else if (imc > 18.5 && imc <= 24.9){
         resultado.innerHTML = "IMC: " + imc.toFixed(2) + "<br> Seu peso está ok."
     }
-    else if (imc > 24 && imc <= 29){
+    else if (imc > 24.9 && imc <= 29.9){
         resultado.innerHTML = "IMC: " + imc.toFixed(2) + "<br> Você está com sobrepeso."
     }
-    else if (imc > 30 && imc <= 40){
-        resultado.innerHTML = "IMC: " + imc.toFixed(2) + "<br> Você está obeso."
+    else if (imc > 29.9 && imc <= 34.9){
+        resultado.innerHTML = "IMC: " + imc.toFixed(2) + "<br> Obesidade nível I."
+    }
+    else if (imc > 34.9 && imc <= 40){
+        resultado.innerHTML = "IMC: " + imc.toFixed(2) + "<br> Obesidade nível II."
     }
     else if (imc > 40){
-        resultado.innerHTML = "IMC: " + imc.toFixed(2) + "<br> Você está muito acima do peso."
+        resultado.innerHTML = "IMC: " + imc.toFixed(2) + "<br> Obesidade nível III."
     }
     document.getElementById("peso").value = "";
     document.getElementById("altura").value = "";
